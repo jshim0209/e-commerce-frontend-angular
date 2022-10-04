8e3cd5ebda6d247ada0b9bb1b5b71455e451b15d
Jiwon Shim <jshim0209@hotmail.com>
2022-10-04 11:33:34 -0400

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartDetailsComponent } from './cart-details/cart-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartDetailsComponent,
+    CartStatusComponent,
+    CheckoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
